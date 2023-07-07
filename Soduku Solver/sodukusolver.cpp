18f274646fc1e73837a87ecfb63ee15b8c4da71c
codebreaker4O4 <42467268+codebreaker4O4@users.noreply.github.com>
2023-07-07 10:55:38 +0530

    Update sodukusolver.cpp

--- Soduku Solver/sodukusolver.cpp
+++ Soduku Solver/sodukusolver.cpp
@@ -8,8 +8,8 @@ using namespace std;
 int main(){
     std::ios_base::sync_with_stdio(false);
     int n;
-    vector<vector<int>> soduku(n, vector<int>(n));
-    soduku={
+    vector<vector<int>> board(n, vector<int>(n));
+    board={
         {1,2,3,4,5,6,7,8,9},
         {2,3,4,5,6,7,8,9,1},
         {3,4,5,6,7,8,9,1,2},
