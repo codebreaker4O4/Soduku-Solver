1e03fc1a6d5db7c32165f939604af6521a390c31
codebreaker4O4 <42467268+codebreaker4O4@users.noreply.github.com>
2023-07-07 10:53:35 +0530

    Add files via upload

---
+++ Soduku Solver/sodukusolver.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define endl '\n'
+
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    int n;
+    vector<vector<int>> soduku(n, vector<int>(n));
+    soduku={
+        {1,2,3,4,5,6,7,8,9},
+        {2,3,4,5,6,7,8,9,1},
+        {3,4,5,6,7,8,9,1,2},
+        {4,5,6,7,8,9,1,2,3},
+        {5,6,7,8,9,1,2,3,4},
+        {6,7,8,9,1,2,3,4,5},
+        {7,8,9,1,2,3,4,5,6},
+        {8,9,1,2,3,4,5,6,7},
+        {9,1,2,3,4,5,6,7,8},
+    }
+    return 0;
+}
